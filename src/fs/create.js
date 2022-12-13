34601987aff34b561ce148a678c46f93c8e6af42
Maksim Leskin <maksim.leskin@methed.ru>
2022-12-13 05:21:24 +0300

    fs-create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'node:fs/promises';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(`${__dirname}/files/fresh.txt`, 'I am fresh and young', {
+      encoding: 'utf8',
+      flag: 'wx',
+    }).catch(() => {
+        throw new Error('FS operation failed')
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await create();
