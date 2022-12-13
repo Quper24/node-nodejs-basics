3ee7a2c46b7e4e25aca14653b92cbd968a7940ec
Maksim Leskin <quper24@mail.ru>
2022-12-14 01:15:50 +0300

    rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.rename(
+      `${__dirname}/files/wrongFilename.txt`,
+      `${__dirname}/files/properFilenameс.md`,
+    ).catch(() => {
+      throw new Error('FS operation failed');
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await rename();
