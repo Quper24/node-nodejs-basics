0c75a8e630857f40426350a5963880e26a8b3c98
Maksim Leskin <maksim.leskin@methed.ru>
2022-12-13 05:49:34 +0300

    copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    readdir(`${__dirname}/files`)
+      .then(async (files) => {
+        if (files.length) {
+          await mkdir(`${__dirname}/files_copy`);
+          return files;
+        }
+      })
+      .then((files) => {
+        files.forEach(async (file) => {
+          await copyFile(
+            `${__dirname}/files/${file}`,
+            `${__dirname}/files_copy/${file}`,
+          );
+        });
+      })
+      .catch((e) => {
+        console.log('e: ', e);
+        throw new Error('FS operation failed');
+      });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await copy();
