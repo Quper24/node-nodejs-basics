802f12a431287216f320a76efef8f8eec4ae83a2
Maksim Leskin <maksim.leskin@skillbox.ru>
2022-12-15 20:49:39 +0300

    add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const hash = createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+  hash.on('readable', () => {
+    const data = hash.read();
+    if (data) {
+      console.log(data.toString('hex'));
+    }
+  });
+
+  const textFile = await readFile(
+    `${__dirname}/files/fileToCalculateHashFor.txt`,
+    {
+      encoding: 'utf8',
+    },
+  );
+
+  hash.write(textFile);
+  hash.end();
 };
 
 await calculateHash();
